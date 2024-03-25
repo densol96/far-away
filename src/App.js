d6fa2e18d4ffc36256195a0e901aebd29209fbc6
Deniss Solovjovs <deniss11sol@gmail.com>
2024-03-26 00:12:57 +0200

    derived state explored

--- src/App.js
+++ src/App.js
@@ -1,14 +1,34 @@
-const initialItems = [
-  { id: 1, description: 'Passports', quantity: 2, packed: true },
-  { id: 2, description: 'Socks', quantity: 12, packed: false },
-];
+import { useState } from 'react';
 
 export default function App() {
+  const [items, setItems] = useState([]);
+
+  function handleAddItems(item) {
+    setItems((items) => [...items, item]);
+  }
+
+  function deleteAddItems(id) {
+    setItems((items) => items.filter((item) => item.id !== id));
+  }
+
+  function toggleCheckBox(id) {
+    setItems((items) =>
+      // eslint-disable-next-line no-confusing-arrow
+      items.map((item) =>
+        item.id === id ? { ...item, packed: !item.packed } : item
+      )
+    );
+  }
+
   return (
     <div className="app">
       <Logo />
-      <Form />
-      <PackingList />
+      <Form onAddItems={handleAddItems} />
+      <PackingList
+        items={items}
+        onDeleteItem={deleteAddItems}
+        onToggleCheckbox={toggleCheckBox}
+      />
       <Stats />
     </div>
   );
@@ -18,35 +38,81 @@ function Logo() {
   return <h1>🌴 Far Away 🛍️</h1>;
 }
 
-function Form() {
+function Form({ onAddItems }) {
+  const [description, setDescription] = useState('');
+  const [quantity, setQuantity] = useState(1);
+
+  function handleSubmbit(e) {
+    e.preventDefault();
+    if (!description) return alert('Form is incomplete!');
+
+    const newItem = {
+      quantity,
+      description,
+      packed: false,
+      id: Date.now(),
+    };
+
+    onAddItems(newItem);
+    // Clear the form
+    setDescription('');
+    setQuantity(1);
+  }
+
   return (
-    <div className="add-form">
+    <form className="add-form" onSubmit={handleSubmbit}>
       <h3>What do you need for your 😍 trip?</h3>
-    </div>
+      <select
+        value={quantity}
+        onChange={(e) => setQuantity(Number(e.target.value))}
+      >
+        {Array.from({ length: 20 }, (_, i) => i + 1).map((num) => (
+          <option value={num} key={num}>
+            {num}
+          </option>
+        ))}
+      </select>
+      <input
+        type="text"
+        placeholder="Item..."
+        value={description}
+        onChange={(e) => setDescription(e.target.value)}
+      ></input>
+      <button>Add</button>
+    </form>
   );
 }
 
-function PackingList() {
+function PackingList({ items, onDeleteItem, onToggleCheckbox }) {
   return (
     <div className="list">
       <ul>
-        {initialItems.map((item) => (
-          <Item item={item} />
+        {items.map((item) => (
+          <Item
+            item={item}
+            onDeleteItem={onDeleteItem}
+            onToggleCheckbox={onToggleCheckbox}
+            key={item.id}
+          />
         ))}
       </ul>
     </div>
   );
 }
 
-function Item({ item }) {
+function Item({ item, onDeleteItem, onToggleCheckbox }) {
   const { id, description, quantity, packed } = item;
-
   return (
     <li>
+      <input
+        type="checkbox"
+        checked={packed}
+        onChange={() => onToggleCheckbox(id)}
+      ></input>
       <span style={packed ? { textDecoration: 'line-through' } : {}}>
         {quantity} {description}
       </span>
-      <button>❌</button>
+      <button onClick={() => onDeleteItem(id)}>❌</button>
     </li>
   );
 }
